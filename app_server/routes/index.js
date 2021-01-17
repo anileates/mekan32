bc9b278ccef5f2c7a5259f91e8e3e065d59fa0ad
atesanilerdem <49078844+atesanilerdem@users.noreply.github.com>
2021-01-17 15:03:08 +0300

    Deneme

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,14 +5,16 @@ var ctrlDigerleri=require('../controllers/digerleri');
 
 router.get('/', ctrlMekanlar.anaSayfa);
 router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
-// router.get(, ); // yorum ekle sayfasını çağırır
-// router.post('/mekan/:mekanid/yorum/yeni', ); // yorum ekle sayfasında yorum ekle butonuna basılınca bu çalışır
 
+router.get('/mekan/:mekanid/yorum/yeni', ctrlMekanlar.yorumEkle); // yorum ekle sayfasını çağırır
+router.post('/mekan/:mekanid/yorum/yeni', ctrlMekanlar.yorumumuEkle); // yorum ekle sayfasında yorum ekle butonuna basılınca bu çalışır
+
+/** Ortak yolları bu şekilde de tanımlayabilirsin. 
 router
 .route('/mekan/:mekanid/yorum/yeni')
 .get(ctrlMekanlar.yorumEkle)
 .post(ctrlMekanlar.yorumumuEkle)
-
+*/
 
 router.get('/hakkinda', ctrlDigerleri.hakkinda);
 module.exports = router;
